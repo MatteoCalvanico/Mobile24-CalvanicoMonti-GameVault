a5874f7dfd55326677758e7bcf58e826d59d8dc2
Matteo <matteo.calvanico@studio.unibo.it>
2024-05-09 20:20:55 +0200

    Vault imageButton now work, add a textView if there is no game

--- GameVault/app/src/main/java/it/unibo/gamevault/ui/HomePageActivity.kt
+++ GameVault/app/src/main/java/it/unibo/gamevault/ui/HomePageActivity.kt
@@ -1,12 +1,25 @@
 package it.unibo.gamevault.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import it.unibo.gamevault.R
 
 class HomePageActivity : AppCompatActivity() {
+
+    private lateinit var btnVault: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        btnVault = findViewById(R.id.btnVault)
+
+        // Click vault imageButton
+        btnVault.setOnClickListener{
+            val intent = Intent(this, VaultActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- GameVault/app/src/main/java/it/unibo/gamevault/ui/VaultActivity.kt
+++ GameVault/app/src/main/java/it/unibo/gamevault/ui/VaultActivity.kt
@@ -2,11 +2,36 @@ package it.unibo.gamevault.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import it.unibo.gamevault.R
+import it.unibo.gamevault.ui.adapter.VaultGamesAdapter
+import it.unibo.gamevault.ui.model.VaultGamesModel
 
 class VaultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vault_recycler)
+
+        val txtNoGame: TextView = findViewById(R.id.txtNoGame)
+
+        /**
+         * Prendere i dati dei giochi salvati dal database e salvarli in lista
+         */
+        val vaultGames = listOf<VaultGamesModel>() //TODO: gestire il take dei dati dal db
+
+        val vaultGamesAdapter = VaultGamesAdapter(vaultGames)
+
+        //If the vault is empty (no games added) change the textView visibility
+        if (vaultGames.isEmpty()){
+            txtNoGame.visibility = TextView.VISIBLE
+        } else {
+            txtNoGame.visibility = TextView.INVISIBLE
+
+            val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            recyclerView.adapter = vaultGamesAdapter
+        }
     }
 }
