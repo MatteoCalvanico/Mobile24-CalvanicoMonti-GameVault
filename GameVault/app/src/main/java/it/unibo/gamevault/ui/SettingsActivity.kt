c925106bc608f0048bf9db480d5f64649983b693
Matteo <matteo.calvanico@studio.unibo.it>
2024-05-11 16:03:25 +0200

    Add in the settings activity the selection of a profile image

--- GameVault/app/src/main/java/it/unibo/gamevault/ui/SettingsActivity.kt
+++ GameVault/app/src/main/java/it/unibo/gamevault/ui/SettingsActivity.kt
@@ -1,15 +1,37 @@
 package it.unibo.gamevault.ui
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import it.unibo.gamevault.Manifest
+import it.unibo.gamevault.databinding.ActivitySettingBinding
 import it.unibo.gamevault.R
 
 class SettingsActivity : AppCompatActivity() {
-
     private lateinit var btnCancel: Button
     private lateinit var btnSave: Button
+    private lateinit var btnSelectImg: Button
+    private lateinit var imagePreview: ImageView
+
+    //TODO: Remember to save the uri in the app's database
+    private var galleryUri: Uri? = null
+    private var REQUEST_READ_MEDIA_IMAGES = 100
+
+    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        galleryUri = it
+        try {
+            imagePreview.setImageURI(galleryUri)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +39,13 @@ class SettingsActivity : AppCompatActivity() {
 
         btnCancel = findViewById(R.id.btnCancel)
         btnSave = findViewById(R.id.btnSave)
+        btnSelectImg = findViewById(R.id.btnSelectImage)
+        imagePreview = findViewById(R.id.previewImage)
+
+        //Click on select image
+        btnSelectImg.setOnClickListener{
+            galleryLauncher.launch("image/*")
+        }
 
         //Click on save settings button
         btnSave.setOnClickListener{
