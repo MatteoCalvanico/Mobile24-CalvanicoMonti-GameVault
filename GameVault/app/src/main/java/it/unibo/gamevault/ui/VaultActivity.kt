1fb62ffa1df2b2ad70818720cabb1dd534c8b06d
Matteo <matteo.calvanico@studio.unibo.it>
2024-05-16 18:26:15 +0200

    Modify layout

--- GameVault/app/src/main/java/it/unibo/gamevault/ui/VaultActivity.kt
+++ GameVault/app/src/main/java/it/unibo/gamevault/ui/VaultActivity.kt
@@ -8,13 +8,11 @@ import androidx.recyclerview.widget.RecyclerView
 import it.unibo.gamevault.R
 import it.unibo.gamevault.ui.adapter.VaultGamesAdapter
 import it.unibo.gamevault.ui.model.VaultGamesModel
-import java.time.LocalDate
-import java.util.Date
 
 class VaultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_vault_recycler)
+        setContentView(R.layout.activity_vault)
 
         val txtNoGame: TextView = findViewById(R.id.txtNoGame)
 
