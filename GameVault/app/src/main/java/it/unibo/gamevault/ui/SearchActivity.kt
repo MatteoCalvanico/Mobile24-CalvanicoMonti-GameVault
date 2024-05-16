e3b1ea57b1ab2f1f3919db719b03fcebb28cc492
Matteo <matteo.calvanico@studio.unibo.it>
2024-05-16 18:18:21 +0200

    Add SearchGamesAdapter

--- GameVault/app/src/main/java/it/unibo/gamevault/ui/SearchActivity.kt
+++ GameVault/app/src/main/java/it/unibo/gamevault/ui/SearchActivity.kt
@@ -3,18 +3,26 @@ package it.unibo.gamevault.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioGroup
-import android.widget.SearchView
+import android.util.Log;
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import it.unibo.gamevault.R
 import it.unibo.gamevault.ui.adapter.SearchGamesAdapter
+import it.unibo.gamevault.ui.model.Game
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SearchActivity : AppCompatActivity() {
 
     private lateinit var searchBar: SearchView
     private lateinit var searchOptions: RadioGroup
-    //private lateinit var searchAdapter: SearchGamesAdapter
+    private lateinit var recyclerView: RecyclerView
+
+    private lateinit var searchAdapter: SearchGamesAdapter
+    private var searchResults: List<Game> = emptyList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +30,32 @@ class SearchActivity : AppCompatActivity() {
 
         searchBar = findViewById(R.id.searchBar)
         searchOptions = findViewById(R.id.searchOptions)
+        recyclerView = findViewById(R.id.recycler_view)
 
-        //TODO: adapter e sue chiamate
+        searchAdapter = SearchGamesAdapter(searchResults)
+        recyclerView.adapter = searchAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String?): Boolean {
                 query?.let{
+
+                    val queryType = searchOptions.checkedRadioButtonId //Save the type of query
                     CoroutineScope(Dispatchers.IO).launch {
-                        //TODO: fare la chiamata API
-                        /*
-                        * Es:
-                        * val searchResult = 'chiamata API con metodo che ritorna una lista'
-                        *
-                        * withContext(Dispatchers.Main) {
-                        *    searchAdapter.updateSearchResults(searchResults) 'Metodo per aggiornare i dati del recycler'
-                        * }
-                        * */
+
+                        val apiResult: List<Game> = emptyList() //TODO: spostare l'inizializzazione con la chiamata API
+
+                        //We do a different query based on the radioButton checked
+                        when(queryType){ //TODO: fare la chiamata API
+                            R.id.optionSeries -> Log.d("SEARCH_BY_SERIES","Search series")
+                            R.id.optionGame -> Log.d("SEARCH_BY_GAME","Search game")
+                            R.id.optionGame -> Log.d("SEARCH_BY_MAP","Search map")
+                        }
+
+                        withContext(Dispatchers.Main){
+                            searchResults = apiResult
+                            searchAdapter.updateSearchResults(searchResults)
+                        }
                     }
                 }
                 return true
---
+++ GameVault/app/src/main/java/it/unibo/gamevault/ui/adapter/SearchGamesAdapter.kt
@@ -0,0 +1,43 @@
+package it.unibo.gamevault.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import it.unibo.gamevault.R
+import it.unibo.gamevault.ui.model.Game
+
+class SearchGamesAdapter(private var dataSet: List<Game>) : RecyclerView.Adapter<SearchGamesAdapter.GameViewHolder>() {
+
+    class GameViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val gameName: TextView = itemView.findViewById(R.id.txtGameName)
+        val gameRelease: TextView = itemView.findViewById(R.id.txtGameYear)
+        val gamePoster: ImageView = itemView.findViewById(R.id.searchGamePoster)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.search_item, parent, false)
+        return GameViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
+        val currentGame = dataSet[position]
+
+        holder.gameName.text = currentGame.name
+        holder.gameRelease.text = currentGame.released
+        Glide.with(holder.itemView)
+            .load(currentGame.backgroundImage)
+            .placeholder(R.drawable.poster_placeholder)
+            .into(holder.gamePoster)
+    }
+
+    override fun getItemCount(): Int = dataSet.size
+
+    fun updateSearchResults(newResults: List<Game>) {
+        dataSet = newResults
+        notifyDataSetChanged()
+    }
+}
