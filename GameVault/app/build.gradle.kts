610b9e981edcdcce77133306bb909f9b0a87121a
Matteo <matteo.calvanico@studio.unibo.it>
2024-05-17 14:59:01 +0200

    Add Moshi and Retrofit

--- GameVault/app/build.gradle.kts
+++ GameVault/app/build.gradle.kts
@@ -45,11 +45,22 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference:1.2.1")
-    testImplementation("junit:junit:4.13.2")
+    implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.cardview:cardview:1.0.0")
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.google.android.material:material:1.8.0")
+
+    //Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    //Moshi
+    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
+
+    //Retrofit with Moshi converter
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 }
