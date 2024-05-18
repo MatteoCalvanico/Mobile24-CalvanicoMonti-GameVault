330afa967937bca12c3e38424bdb8ad5e7d78a2a
Matteo <matteo.calvanico@studio.unibo.it>
2024-05-18 16:41:30 +0200

    Modify RemoteService

--- GameVault/app/src/main/java/it/unibo/gamevault/data/sources/RemoteService.kt
+++ GameVault/app/src/main/java/it/unibo/gamevault/data/sources/RemoteService.kt
@@ -1,33 +1,23 @@
 package it.unibo.gamevault.data.sources
 
 import it.unibo.gamevault.data.sources.remotemodels.games.GamesRemoteModel
-import it.unibo.gamevault.data.sources.remotemodels.games.GamesSeriesRemoteModel
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RemoteService {
 
     //Take the single game
-    @Headers(
-        "X-RapidAPI-Key: 9f5975eda66a400b90008b124bc28eb9",
-        "X-RapidAPI-Host: https://rawg.io/"
-    )
-    @GET("games")
+    @GET("games/{id}")
     suspend fun getGame(
-        @Query("id") id: String,
+        @Path("id") id: String,
         @Query("key") apiKey: String = GamesApi.API_KEY
     ): GamesRemoteModel
 
     //Take all the game from the series
-    @Headers(
-        "X-RapidAPI-Key: 9f5975eda66a400b90008b124bc28eb9",
-        "X-RapidAPI-Host: https://rawg.io/"
-    )
-    @GET("{game_pk}/game-series")
+    @GET("games/{game_pk}/game-series")
     suspend fun getGameSeries(
         @Path("game_pk") gamePk: String,
         @Query("key") apiKey: String = GamesApi.API_KEY
-    ): GamesSeriesRemoteModel
+    ): List<GamesRemoteModel>
 }
