d5efbf224b11a601b5d4a8188d9038019d523c4c
Filippo Monti <filippo.monti15@studio.unibo.it>
2024-05-23 17:59:29 +0200

    created Database

---
+++ GameVault/app/src/main/java/it/unibo/gamevault/data/local/AppDatabase.kt
@@ -0,0 +1,34 @@
+package it.unibo.gamevault.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import it.unibo.gamevault.data.local.entity.User
+import it.unibo.gamevault.data.local.entity.Game
+import it.unibo.gamevault.data.local.dao.UserDao
+import it.unibo.gamevault.data.local.dao.GameDao
+
+// Annotates class to be a Room Database with a table (entity) of the Word class
+@Database(entities = [User::class, Game::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+    abstract fun gameDao(): GameDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "gameVault_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
