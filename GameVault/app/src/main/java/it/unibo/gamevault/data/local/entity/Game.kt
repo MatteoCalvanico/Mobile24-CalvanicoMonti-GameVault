1c99624c94130989d4e6212c3d7f2cd83d42fb3c
Filippo Monti <filippo.monti15@studio.unibo.it>
2024-05-23 16:40:14 +0200

    created the game table

---
+++ GameVault/app/src/main/java/it/unibo/gamevault/data/local/entity/Game.kt
@@ -0,0 +1,15 @@
+package it.unibo.gamevault.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "game")
+data class Game(
+    @PrimaryKey val slug: String,
+    @ColumnInfo(name = "img_link") val imgLink: String?,
+    @ColumnInfo(name = "game_name") val gameName: String?,
+    @ColumnInfo(name = "your_rating") val yourRating: Double?,
+    @ColumnInfo(name = "start_date") val startDate: String?,
+    @ColumnInfo(name = "end_date") val endDate: String?
+)
