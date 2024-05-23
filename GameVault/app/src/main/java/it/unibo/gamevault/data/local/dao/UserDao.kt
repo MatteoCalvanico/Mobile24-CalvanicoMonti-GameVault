699c42361d5c19a7b7756bba10258ad5f9439d28
Filippo Monti <filippo.monti15@studio.unibo.it>
2024-05-23 17:01:59 +0200

    created the UserDao

---
+++ GameVault/app/src/main/java/it/unibo/gamevault/data/local/dao/UserDao.kt
@@ -0,0 +1,28 @@
+package it.unibo.gamevault.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.google.firebase.firestore.auth.User
+
+
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    suspend fun getAllUsers(): List<User>
+
+    @Query("SELECT * FROM user WHERE id = :id")
+    suspend fun getUserById(id: Int): User?
+
+    @Insert
+    suspend fun insertUser(user: User)
+
+    @Update
+    suspend fun updateUser(user: User)
+
+    @Delete
+    suspend fun deleteUser(user: User)
+}
