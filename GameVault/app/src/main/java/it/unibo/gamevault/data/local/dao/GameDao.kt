4618257372d911d8f37dafaa91f90786339029ac
Filippo Monti <filippo.monti15@studio.unibo.it>
2024-05-26 14:49:02 +0200

    add 'flow' and removed 'suspend' in getAllGames

--- GameVault/app/src/main/java/it/unibo/gamevault/data/local/dao/GameDao.kt
+++ GameVault/app/src/main/java/it/unibo/gamevault/data/local/dao/GameDao.kt
@@ -6,22 +6,24 @@ import androidx.room.Query
 import androidx.room.Update
 import androidx.room.Delete
 import androidx.room.OnConflictStrategy
-import it.unibo.gamevault.data.local.entity.Game
+import it.unibo.gamevault.data.local.entity.GameLocalModel
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface GameDao {
     @Query("SELECT * FROM game")
-    suspend fun getAllGames(): List<Game>
+    fun getAllGames(): Flow<List<GameLocalModel>>//uso flow perchè mi serve in UserRepository
+//ho tolto 'suspend' perchè in 'UserRepository' questa funzione deve essere richiamata direttamente fuori da una coroutine o un'altra funzione di sospensione
 
     @Query("SELECT * FROM game WHERE slug = :slug")
-    suspend fun getGameBySlug(slug: String): Game?
+    suspend fun getGameBySlug(slug: String): GameLocalModel?
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertGame(game: Game)
+    suspend fun insertGame(game: GameLocalModel)
 
     @Update
-    suspend fun updateGame(game: Game)
+    suspend fun updateGame(game: GameLocalModel)
 
     @Delete
-    suspend fun deleteGame(game: Game)
+    suspend fun deleteGame(game: GameLocalModel)
 }
