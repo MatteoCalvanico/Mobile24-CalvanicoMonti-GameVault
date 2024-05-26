55071ee1e3f1fd8e9782223de3ebe264d98710cb
Filippo Monti <filippo.monti15@studio.unibo.it>
2024-05-26 14:43:41 +0200

    add 'flow' and removed 'suspend' in getAllUsers

--- GameVault/app/src/main/java/it/unibo/gamevault/data/local/dao/UserDao.kt
+++ GameVault/app/src/main/java/it/unibo/gamevault/data/local/dao/UserDao.kt
@@ -6,24 +6,24 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
-import com.google.firebase.firestore.auth.User
-
+import it.unibo.gamevault.data.local.entity.UserLocalModel
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface UserDao {
     @Query("SELECT * FROM user")
-    suspend fun getAllUsers(): List<User>
-
+    fun getAllUsers(): Flow<List<UserLocalModel>>//uso flow perchè mi serve in UserRepository
+//ho tolto 'suspend' perchè in 'UserRepository' questa funzione deve essere richiamata direttamente fuori da una coroutine o un'altra funzione di sospensione
     @Query("SELECT * FROM user WHERE id = :id")
-    suspend fun getUserById(id: Int): User?
+    suspend fun getUserById(id: Int): UserLocalModel?
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertUser(user: User)
+    suspend fun insertUser(user: UserLocalModel)
 
     @Update
-    suspend fun updateUser(user: User)
+    suspend fun updateUser(user: UserLocalModel)
 
     @Delete
-    suspend fun deleteUser(user: User)
+    suspend fun deleteUser(user: UserLocalModel)
 }
