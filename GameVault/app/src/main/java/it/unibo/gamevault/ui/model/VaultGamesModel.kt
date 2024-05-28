477455b6b95a17d14e26ea7908f814cd5479ce18
Matteo <matteo.calvanico@studio.unibo.it>
2024-05-28 20:53:05 +0200

    Change model to Parcelize

--- GameVault/app/src/main/java/it/unibo/gamevault/ui/model/VaultGamesModel.kt
+++ GameVault/app/src/main/java/it/unibo/gamevault/ui/model/VaultGamesModel.kt
@@ -1,10 +1,12 @@
 package it.unibo.gamevault.ui.model
 
-import java.util.Date
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
 /**
  * Represent how the game in the vault are saved
  */
 
 //The rating and the two dates are not mandatory
-data class VaultGamesModel (val imgLink : String, val gameName : String, val yourRating : Double?, val startDate : String?, val endDate : String?)
+@Parcelize
+data class VaultGamesModel (val imgLink : String, val gameName : String, val yourRating : Double?, val startDate : String?, val endDate : String?) : Parcelable
