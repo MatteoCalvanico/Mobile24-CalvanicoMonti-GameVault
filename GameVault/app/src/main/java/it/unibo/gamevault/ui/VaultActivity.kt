17b1227bc7fca346e9974217962613a53e537bda
Filippo Monti <filippo.monti15@studio.unibo.it>
2024-05-28 15:51:05 +0200

    update VaultActivity

--- GameVault/app/src/main/java/it/unibo/gamevault/ui/VaultActivity.kt
+++ GameVault/app/src/main/java/it/unibo/gamevault/ui/VaultActivity.kt
@@ -3,35 +3,44 @@ package it.unibo.gamevault.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import it.unibo.gamevault.R
 import it.unibo.gamevault.ui.adapter.VaultGamesAdapter
 import it.unibo.gamevault.ui.model.VaultGamesModel
+import it.unibo.gamevault.ui.viewModel.VaultViewModel
 
 class VaultActivity : AppCompatActivity() {
+
+    private val vaultViewModel: VaultViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vault)
 
         val txtNoGame: TextView = findViewById(R.id.txtNoGame)
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        /**
-         * Prendere i dati dei giochi salvati dal database e salvarli in lista
-         */
-        val vaultGames = listOf<VaultGamesModel>(VaultGamesModel("https://media.rawg.io/media/screenshots/623/623973e168a64a1f351a429d1f4db46c_iMXtnmX.jpg", "The Witcher: Rise of the white wolf", 4.0, "00/00/2020", "00/00/2020")) //TODO: gestire il take dei dati dal db
-
-        val vaultGamesAdapter = VaultGamesAdapter(vaultGames)
-
-        //If the vault is empty (no games added) change the textView visibility
-        if (vaultGames.isEmpty()){
-            txtNoGame.visibility = TextView.VISIBLE
-        } else {
-            txtNoGame.visibility = TextView.INVISIBLE
-
-            val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
-            recyclerView.layoutManager = LinearLayoutManager(this)
-            recyclerView.adapter = vaultGamesAdapter
+        vaultViewModel.combinedGamesVault.observe(this) { games ->
+            if (games.isEmpty()) {
+                txtNoGame.visibility = TextView.VISIBLE
+            } else {
+                txtNoGame.visibility = TextView.INVISIBLE
+                val vaultGames = games.map { game ->
+                    VaultGamesModel(
+                        imgLink = game.imgLink ?: "",
+                        gameName = game.gameName ?: "No game",
+                        yourRating = game.yourRating ?: 0.0,
+                        startDate = game.startDate ?: "0/0/0000",
+                        endDate = game.endDate ?: "0/0/0000"
+                    )
+                }
+                val vaultGamesAdapter = VaultGamesAdapter(vaultGames)
+                recyclerView.adapter = vaultGamesAdapter
+            }
         }
     }
 }
