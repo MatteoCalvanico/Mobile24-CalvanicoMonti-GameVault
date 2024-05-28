c94e8f09712de8b6ce9e3f7289d9bd4dc5f1b9f9
Matteo <matteo.calvanico@studio.unibo.it>
2024-05-28 12:02:24 +0200

    Add loadUserData and saveUserData in SettingsActivity, problem with the load of pfp

--- GameVault/app/src/main/java/it/unibo/gamevault/ui/SettingsActivity.kt
+++ GameVault/app/src/main/java/it/unibo/gamevault/ui/SettingsActivity.kt
@@ -4,18 +4,27 @@ import android.Manifest
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.database.sqlite.SQLiteConstraintException
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import it.unibo.gamevault.R
+import it.unibo.gamevault.data.local.Repository.AppRepository
+import it.unibo.gamevault.data.local.entity.UserLocalModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var btnCancel: Button
@@ -23,6 +32,13 @@ class SettingsActivity : AppCompatActivity() {
     private lateinit var btnSelectImg: Button
     private lateinit var imagePreview: ImageView
     private lateinit var btnRawg: Button
+    private lateinit var editPSN: EditText
+    private lateinit var editSteam: EditText
+    private lateinit var editXbox: EditText
+    private lateinit var editRawg: EditText
+
+    private lateinit var repository: AppRepository
+    private var userInfo: UserLocalModel? = null
 
     //TODO: Remember to save the uri in the app's database
     private var galleryUri: Uri? = null
@@ -46,6 +62,13 @@ class SettingsActivity : AppCompatActivity() {
         btnSelectImg = findViewById(R.id.btnSelectImage)
         imagePreview = findViewById(R.id.previewImage)
         btnRawg = findViewById(R.id.btnRawg)
+        editPSN = findViewById(R.id.editPSN)
+        editSteam = findViewById(R.id.editSteam)
+        editXbox = findViewById(R.id.editXbox)
+        editRawg = findViewById(R.id.editRawg)
+
+        repository = (application as it.unibo.gamevault.Application).repository
+        loadUserData()
 
         //Click on "go to Rawg"
         btnRawg.setOnClickListener {
@@ -63,19 +86,72 @@ class SettingsActivity : AppCompatActivity() {
         //Click on select image
         btnSelectImg.setOnClickListener {
             if (checkAndRequestGalleryPermission()) {
-                galleryLauncher.launch("image/*")
+                galleryLauncher.launch("image/*") //TODO: Usare Gidle
             }
         }
 
         //Click on save settings button
         btnSave.setOnClickListener{
-            //TODO
+            saveUserInfo()
         }
 
         //Click on cancel setting button (go back to the home)
         btnCancel.setOnClickListener{
             val intent = Intent(this, HomePageActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    //Load the old profile info
+    private fun loadUserData() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val user = repository.getUserById(0)
+            userInfo = user
+
+            withContext(Dispatchers.Main){
+                if (user != null) {
+                    Toast.makeText(this@SettingsActivity, "Profile found, info loaded", Toast.LENGTH_SHORT).show()
+                    editPSN.setText(user.PSNLink)
+                    editSteam.setText(user.steamLink)
+                    editXbox.setText(user.XboxLink)
+                    editRawg.setText(user.APIKey)
+                    Glide.with(this@SettingsActivity)
+                        .load(user.profileImage?.let { uri -> Uri.parse(uri) })
+                        .into(imagePreview)
+                }else {
+                    Toast.makeText(this@SettingsActivity, "No profile found", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun saveUserInfo() {
+        val user = UserLocalModel(
+            0,
+            galleryUri?.toString() ?: userInfo?.profileImage.orEmpty(),
+            editPSN.text.toString().ifEmpty { userInfo?.PSNLink.orEmpty() },
+            editSteam.text.toString().ifEmpty { userInfo?.steamLink.orEmpty() },
+            editXbox.text.toString().ifEmpty { userInfo?.XboxLink.orEmpty() },
+            editRawg.text.toString().ifEmpty { userInfo?.APIKey.orEmpty() },
+            null,
+            null,
+            null,
+            null //TODO: gestione preferiti
+        )
+
+        try {
+            CoroutineScope(Dispatchers.IO).launch {
+                if (userInfo == null) {
+                    repository.insertUser(user)
+                } else {
+                    repository.updateUser(user)
+                }
+            }
+            Toast.makeText(this@SettingsActivity, "Info saved", Toast.LENGTH_SHORT).show()
+        } catch (e: SQLiteConstraintException) {
+            Toast.makeText(this@SettingsActivity, "SQL Error", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Toast.makeText(this@SettingsActivity, "Unexpected Error", Toast.LENGTH_SHORT).show()
         }
     }
 
