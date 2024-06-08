232fa7ba8f63a94d016d7191d56184c0d428640d
Matteo <matteo.calvanico@studio.unibo.it>
2024-06-08 11:31:19 +0200

    Change app version

--- GameVault/app/build.gradle.kts
+++ GameVault/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "it.unibo.gamevault"
         minSdk = 27
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
