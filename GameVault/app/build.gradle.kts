214d29d2985b0c92e873d08ee3a88847066445bf
MatteoCalvanico <matteo.calvanico03@gmail.com>
2024-09-20 17:05:31 +0100

    Change version

--- GameVault/app/build.gradle.kts
+++ GameVault/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 27
         targetSdk = 34
         versionCode = 3
-        versionName = "2"
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
